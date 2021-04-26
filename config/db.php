eaeb12b79819ed1e612fa30d51b0e29f9fc9200d
RicardoGonza1 <76696984+RicardoGonza1@users.noreply.github.com>
2021-04-25 21:28:30 -0500

    Add files via upload

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'root');//Usuario de tu base de datos
-define('DB_PASS', '');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'perfil');//Nombre de la base de datos
+define('DB_HOST', 'bap9wq9a2381ktpyfvde-mysql.services.clever-cloud.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'uiainuzv9ehedcyc');//Usuario de tu base de datos
+define('DB_PASS', 'DCQYtst3hIHvKENPh1B2');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'bap9wq9a2381ktpyfvde');//Nombre de la base de datos
 ?>
